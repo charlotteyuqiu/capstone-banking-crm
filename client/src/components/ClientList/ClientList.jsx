9c6c84b790611f1d3f1a10ba642cb7d5c35b2357
Charlotte <charlotteyuqiu@hotmail.com>
2024-07-10 01:09:59 -0400

    complete client list home page including responsive design

--- client/src/components/ClientList/ClientList.jsx
+++ client/src/components/ClientList/ClientList.jsx
@@ -6,24 +6,25 @@ import EditIcon from "../../assets/icons/edit-24px.svg";
 const ClientList = ({ clients }) => {
   return (
     <div className="client-list">
-      <h2 className="client-list__title">View Client Details</h2>
-      <button className="client-list__add-btn">+ Add New Client</button>
+      <div className="client-list__upper-part">
+        <h2 className="client-list__title">View Client Details</h2>
+        <button className="client-list__add-btn">+ Add New Client</button>
+      </div>
       <ul className="client-list__items">
         {clients.map((client) => (
           <li key={client.client_id} className="client-list__item">
-            <div className="client-list__details">
+            <div className="client-list__card">
+              <div className="client-list__name-edit ">
+                <p className="client-list__text">
+                  <strong>{client.name}</strong>
+                </p>
+                <img
+                  className="client-list__edit-icon"
+                  src={EditIcon}
+                  alt="edit-icon"
+                />
+              </div>
               <div className="client-list__info">
-                <div className="client-list__name-edit">
-                  <p className="client-list__text">
-                    <strong>{client.name}</strong>
-                  </p>
-                  <img
-                    className="client-list__edit-icon"
-                    src={EditIcon}
-                    alt="edit-icon"
-                  />
-                </div>
-
                 <p className="client-list__text">
                   <strong>EMAIL</strong> {client.email}
                 </p>
@@ -31,7 +32,8 @@ const ClientList = ({ clients }) => {
                   <strong>PHONE</strong> {client.phone}
                 </p>
               </div>
-
+            </div>
+            <div className="client-list__portfolio-btn">
               <button className="client-list__portfolio-btn">
                 View Portfolio Details
               </button>
